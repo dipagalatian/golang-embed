046c21f64aa8f6974fc8190febdbef59b6a8968f
dipagalatian <dipagalatian3003@gmail.com>
2026-01-17 20:19:04 +0700

    FEAT: Embed file binary to var slice byte using go embed command

--- embed_test.go
+++ embed_test.go
@@ -3,6 +3,8 @@ package golangembed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"os"
 	"testing"
 )
 
@@ -12,9 +14,20 @@ var version string
 //go:embed version.txt
 var version2 string
 
-func TestEmbed(t *testing.T) {
+func TestEmbedString(t *testing.T) {
 
 	fmt.Println("Version:", version)
 	fmt.Println("Version2:", version2)
+	
+}
+
+//go:embed cat_image.jpg
+var cat_image []byte 
+func TestEmbedByte(t *testing.T) {
+
+	err := os.WriteFile("copy_cat_image.jpg", cat_image, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 	
 }
